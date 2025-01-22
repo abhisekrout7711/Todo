ee4893d740a8f77fcb179f886f1b497981af6ff1
Abhisek Rout <abhisek.rout@impactanalytics.co>
2025-01-23 02:13:27 +0530

    databse and model

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,1 +1,12 @@
+from enum import Enum
 
+class TaskStatus(Enum):
+    Pending = "Pending"
+    Completed = "Completed"
+    InProgress = "In Progress"
+    Overdue = "Overdue"
+
+class TaskPriority(Enum):
+    Low = "Low"
+    Medium = "Medium"
+    High = "High"
