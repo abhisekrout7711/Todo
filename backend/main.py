5f0dd0560d5eedc32bb9794bad92ae1a15d3a972
Abhisek Rout <abhisek.rout@impactanalytics.co>
2025-01-24 00:59:06 +0530

    minor change

--- backend/main.py
+++ backend/main.py
@@ -19,4 +19,4 @@ async def app_start():
 application.include_router(router=router)
 
 if __name__ == "__main__":
-    uvicorn.run("backend.main:app", host="0.0.0.0", port=8080, reload=True)
+    uvicorn.run("backend.main:application", host="0.0.0.0", port=8080, reload=True)
