705343b2a76e3575c63cb5c0a19c89e1bb308d5a
Abhisek Rout <abhisek.rout@impactanalytics.co>
2025-01-26 09:33:30 +0530

    added some doc strings

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -44,6 +44,7 @@ async def login(form_data: OAuth2PasswordRequestForm = Depends()):
 @router.post("/logout", status_code=200)
 @raise_exception
 async def logout(current_user: dict=Depends(get_current_user)):
+    """Revokes the JWT token for the current user"""
     TokenData().revoke_token(current_user["token"])
     return {"message": "Logout successful", "status_code": 200}
 
@@ -51,6 +52,7 @@ async def logout(current_user: dict=Depends(get_current_user)):
 @router.post("/delete", status_code=200)
 @raise_exception
 async def delete_user(current_user: dict=Depends(get_current_user)):
+    """Deletes user and all associated data from the database"""
     response = UserData().delete_user(user_id=current_user["user"].user_id)
     if "error" in response:
         raise HTTPException(status_code=response["status_code"], detail=response["error"])
